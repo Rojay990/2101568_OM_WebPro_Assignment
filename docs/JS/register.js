ec9226f1e06504b90f28988f966f93ad896ca2b3
Your Name <youremail@example.com>
2025-04-11 22:03:33 -0500

    m

--- docs/JS/register.js
+++ docs/JS/register.js
@@ -98,6 +98,6 @@ function resetForm() {
 }
 
 toggleLogin.addEventListener("click", (event) => {
-        window.location.href = "../login.html";
+        window.location.href = "../index.html";
 });
 
